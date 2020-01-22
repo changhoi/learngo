d187e1f3e98a1e720283e28b773987dc4488e70e
changhoi <0417kch@naver.com>
2020-01-22 20:19:19 +0900

    slow url check

--- main.go
+++ main.go
@@ -1,22 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/changhoi/learngo/mydict"
+	"net/http"
 )
 
-func main() {
-	dic := mydict.Dictionary{}
-	base := "hello"
-	dic.Add(base, "first")
-	dic.Update(base, "second")
-	word, _ := dic.Search(base)
-	fmt.Println(word)
+var errRequestFailed = errors.New("Request Fail")
 
-	dic.Delete(base)
-	_, err := dic.Search(base)
-	if err != nil {
-		fmt.Println(err)
+func main() {
+	var results = make(map[string]string)
+	urls := []string{"https://github.com", "https://google.com", "https://amazon.com", "https://facebook.com"}
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
 	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("CHECKING URL")
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
